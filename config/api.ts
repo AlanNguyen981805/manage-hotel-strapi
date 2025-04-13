8b1fb7b30eb1dc6c84bb7f1e80323de48a26666a
Hiep Nguyen Ngoc (SDC3.BU2) <hiep.nguyen5@ntq-solution.com.vn>
2025-04-14 00:26:04 +0700

    config: get all record

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,7 @@
 export default {
   rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
+    defaultLimit: -1, // -1 means no limit, to get all records
+    maxLimit: -1, // Allow unlimited records to be fetched
     withCount: true,
   },
 };
